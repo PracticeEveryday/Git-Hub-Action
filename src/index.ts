b391441e740c49b21069146f6ab69b2dd79e00f9
DongHyun Kim <developerforus@gmail.com>
2022-08-25 13:30:49 +0900

    TypedI test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Macbook, Laptop, Programmer } from "./TypedI/DependencyInjection";
 
+import {
+  programmer_With_TypedI,
+  programmer2_With_TypedI,
+} from "./TypedI/DI.withTypedI";
+
 const programmer: Programmer = new Programmer(new Laptop());
 const programmer2: Programmer = new Programmer(new Macbook());
 
 programmer.programming();
 programmer2.programming();
+
+console.log(programmer_With_TypedI);
+console.log(programmer2_With_TypedI);
